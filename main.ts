0b747e9b814585b18f16e4d0ba42117d91ee55f2
Bcuzeau <73339099+Bcuzeau@users.noreply.github.com>
2022-08-20 11:05:15 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,16 +2,17 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     Temps = -1
 })
 let Temps = 0
-Temps = 10
+Temps = 9
 while (Temps > 0) {
+    basic.showNumber(Temps)
     Temps += -1
     basic.pause(1000)
-    basic.showNumber(Temps)
 }
 if (Temps == -1) {
     basic.showString("gagné")
     music.playMelody("E B C5 A B G A F ", 120)
 } else {
+    basic.clearScreen()
     music.playSoundEffect(music.builtinSoundEffect(soundExpression.soaring), SoundExpressionPlayMode.UntilDone)
     basic.showLeds(`
         # . # . #
