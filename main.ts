dc185a499a753df9ea5f106955555f304ff2ab93
Bcuzeau <73339099+Bcuzeau@users.noreply.github.com>
2022-08-20 11:27:37 +0200

    Misc fixes, better win music

--- main.ts
+++ main.ts
@@ -1,16 +1,17 @@
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     Temps = -1
+    Temps = -1
 })
 let Temps = 0
 Temps = 9
 while (Temps > 0) {
     basic.showNumber(Temps)
     Temps += -1
-    basic.pause(1000)
+    basic.pause(800)
 }
 if (Temps == -1) {
-    basic.showString("gagné")
-    music.playMelody("E B C5 A B G A F ", 120)
+    basic.showString("Bravo")
+    music.playMelody("C5 B A B C5 B C5 - ", 250)
 } else {
     basic.clearScreen()
     music.playSoundEffect(music.builtinSoundEffect(soundExpression.soaring), SoundExpressionPlayMode.UntilDone)
